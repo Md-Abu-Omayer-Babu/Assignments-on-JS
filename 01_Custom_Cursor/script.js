79bc78bc28cccb0b2017ceedf4a8049a639c1653
Md Abu Omayer Babu <abuomayerbabu@gmail.com>
2025-03-29 23:48:20 +0600

    applied random color on hovering

--- 01_Custom_Cursor/script.js
+++ 01_Custom_Cursor/script.js
@@ -1,4 +1,6 @@
+// cursor
 const cursor = document.getElementById("cursor");
+cursor.classList.add("w-6", "h-6", "bg-blue-500", "rounded-full", "fixed", "top-0", "left-0", "pointer-events-none", "transition-transform", "duration-75");
 
 document.addEventListener("mousemove", (e) => {
     cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
@@ -33,4 +35,17 @@ document.addEventListener("click", ()=> {
 })
 
 
+// Headline
+const headline = document.getElementById("custom-cursor");
 
+let previousColor = cursor.style.backgroundColor;
+
+headline.addEventListener("mouseover", (e) => {
+    cursor.classList.add("w-12", "h-12");
+    cursor.style.backgroundColor = generateRandomColor();
+})
+
+headline.addEventListener("mouseout", (e) => {
+    cursor.classList.remove("w-12", "h-12");
+    cursor.style.backgroundColor = previousColor;
+})
